a27a12ec601812b6ca545e4d3cd4e0e309375762
Guiomar Niso <guiomar.niso@ctb.upm.es>
2021-10-18 18:11:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -54,65 +54,8 @@ ifreqs = [i for i, f in zip(range(0, len(freqs)), freqs) if f > fmin  and f < fm
 alpha_freqs = np.take(freqs, ifreqs)
 
 
-
-
-# ==== FIND ALPHA PEAK FREQUENCY ====
-
-alpha_channel_peak = []
-
-# Prepare for Figure 1 containing all the channels
-plt.figure(1)
-fig, axs = plt.subplots(math.ceil(nchannels/15),15, figsize=(50, math.ceil(nchannels/15*2)), facecolor='w', edgecolor='k')
-fig.subplots_adjust(hspace =.5, wspace=.2)
-axs = axs.ravel()
-
-for channel in range(0, nchannels):
-    
-    # Find maxima with noise-tolerant fast peak-finding algorithm
-    psd_channel = np.take(psd_welch[channel, :], ifreqs)
-    pic_loc, pic_mag = mne.preprocessing.peak_finder(psd_channel, extrema=1, verbose=None);
-
-    # From all the peaks found, get the main peak
-
-    #If one peak found
-    if pic_loc.size==1: 
-        peak=pic_loc[0].copy()
-        if peak==0: peak = math.nan # if it's the first value, then ignore, maybe just a decreasing curve
-
-    #If no peak found
-    if pic_loc.size==0: 
-        peak=math.nan #(np.array([0]),np.array([0]))#NaN
-        print('No peak found for channel: ',canales[channel])
-    
-    #If more than one peak found
-    elif pic_loc.size>1:
-        peak = np.where(psd_channel==max(pic_mag))[0][0] # take the max
-        if peak==0: peak = pic_loc[np.argmax(pic_mag[1:,])+1] # if it's the first value, take the next max
-        if peak==psd_channel.size-1: peak = pic_loc[np.argmax(pic_mag[0:-1])] # if it's the last value, take the next max
-
-        print('Multiple peaks found for channel: ',canales[channel])
-
-    #Get the frequency of the peak
-    pic_freq = np.take(alpha_freqs,peak) if not math.isnan(peak) else 0 # to avoid nans
-    alpha_channel_peak.append(pic_freq)
-    
-    # FIGURE 1
-    axs[channel].plot(alpha_freqs,psd_channel);
-    axs[channel].plot(np.take(alpha_freqs,pic_loc),pic_mag,'*');
-    axs[channel].axvline(x=pic_freq,c='k',ls=':');
-    axs[channel].set_title(canales[channel])
-    axs[channel].set_xlim(fmin,fmax)
-
-#Save Figure 1  
-plt.savefig(os.path.join('out_dir2','psd_allchannels.png'),dpi=20)
-plt.close()
-
-'''
-
-# ==== FIND ALPHA MEAN VALUE ====
+# ==== FIND ALPHA MEAN AMPLITUDE VALUE ====
 alpha_channel_peak = np.mean(psd_welch[:,ifreqs], axis=1)
-
-'''
 
 
 # Average value across all channels
@@ -127,25 +70,8 @@ df_alpha.to_csv(os.path.join('out_dir','alpha_peak.csv'))
 
 # ==== PLOT FIGURES ====
 
-# FIGURE 2
-# Plot PSD
-plt.figure(2)
-plt.plot(freqs, psd_welch.transpose(), zorder=1) 
-plt.xlim(xmin=0, xmax=max(freqs))
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Power Spectral Density')
-#plt.title('PSD alpha peak: ',mean_alpha_peak)  
-
-# SHADE ALPHA BAND (GUIO)
-
-plt.axvline(x=mean_alpha_peak,c='k',ls=':')
-# Save fig
-plt.savefig(os.path.join('out_dir2','psd_alpha_peak.png'))
-plt.close()
-
-
-# FIGURE 3
-plt.figure(3)
+# FIGURE 1
+plt.figure(1)
 #custom_params = {"axes.spines.right": False, "axes.spines.top": False}
 #sns.set_theme(style="ticks", rc=custom_params)
 sns.set_theme(style="ticks")
@@ -154,5 +80,5 @@ sns.histplot(data=alpha_channel_peak, binwidth=0.25,kde=True,kde_kws={'cut':10})
 plt.xlabel('Alpha peak frequency (Hz)')
 sns.despine()
 # Save fig
-plt.savefig(os.path.join('out_dir2','hist_alpha_peak.png'))
+plt.savefig(os.path.join('out_figs','hist_alpha_peak.png'))
 plt.close()
