935211f64f38ac9c6b114bc5adbddcd59c8ba9ef
Julia Guiomar Niso Galán <guiomar.niso@ctb.upm.es>
2021-12-01 13:12:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 # Copyright (c) 2020 brainlife.io
 #
-# This file is a MNE python-based brainlife.io App
-#
 # Author: Guiomar Niso
 # Indiana University
 
-# Required libraries
-# pip install mne-bids coloredlogs tqdm pandas scikit-learn json_tricks fire
-
 # set up environment
-#import mne-study-template
 import os
 import json
 import mne
